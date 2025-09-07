f1a0b4d413fc8df61b0d893a8462efd569570e27
Evander Stewart <evanderstewart@gmail.com>
2025-09-07 21:08:12 +0100

    Application class, .gitignore, pom.xml

---
+++ src/main/java/com/se_devops/tocsystem/TocTaskManagerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.se_devops.tocsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TocTaskManagerSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TocTaskManagerSystemApplication.class, args);
+    }
+
+}
