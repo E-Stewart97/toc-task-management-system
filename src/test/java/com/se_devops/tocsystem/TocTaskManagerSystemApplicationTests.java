dc390b914b985872b671250ee715d33df3244937
Evander Stewart <evanderstewart@gmail.com>
2025-09-13 01:52:20 +0100

    TocTaskManagerSystemApplicationTests

---
+++ src/test/java/com/se_devops/tocsystem/TocTaskManagerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se_devops.tocsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TocTaskManagerSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
