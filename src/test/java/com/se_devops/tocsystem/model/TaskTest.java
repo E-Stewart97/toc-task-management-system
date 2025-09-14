841d8d2ac371b832b0d3c9c71b6e08b948a3a7d5
Evander Stewart <evanderstewart@gmail.com>
2025-09-14 01:19:34 +0100

    test

--- src/test/java/com/se_devops/tocsystem/model/TaskTest.java
+++ src/test/java/com/se_devops/tocsystem/model/TaskTest.java
@@ -4,7 +4,7 @@ import com.se_devops.tocsystem.model.enums.TaskPriority;
 import com.se_devops.tocsystem.model.enums.TaskStatus;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class TaskTest {
 
@@ -15,7 +15,7 @@ public class TaskTest {
         task.setTitle("New Important Task");
         task.setStatus(TaskStatus.TODO);
         task.setPriority(TaskPriority.HIGH);
-        task.setDueDate(LocalDate.now().plusDays(7));
+        task.setDueDate(LocalDateTime.now().plusDays(7));
 
         assertEquals(1, task.getId());
         assertEquals("New Important Task", task.getTitle());
